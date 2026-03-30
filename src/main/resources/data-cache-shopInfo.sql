727dc2537168e4e80f3628870e7196c9485c142c
Ellen Developer <ellen.developer@gmx.de>
2026-03-30 10:35:41 +0200

    timestamp testdaten aktualisiert. Logging in logs/bikeparts-app.log

--- src/main/resources/data-cache-shopInfo.sql
+++ src/main/resources/data-cache-shopInfo.sql
@@ -3,5 +3,5 @@
 -- Quelle: https://www.bike-components.de/de/service/versand/
 
 INSERT INTO shop_info (shop_name, shipping_cost_url, shipping_cost, source, fetched_at, FREE_SHIPPING_ON_ORDERS_OVER) VALUES
-('bike-components.de', 'https://www.bike-components.de/de/service/versand/', 4.99, 'WEB_SCRAPING', '2026-03-14 17:32:03', null),
-('bike-discount.de', 'https://www.bike-discount.de/de/shippingcosts', 4.99, 'WEB_SCRAPING', '2026-03-14 17:32:03', 98.99);
+('bike-components.de', 'https://www.bike-components.de/de/service/versand/', 4.99, 'WEB_SCRAPING', '2026-03-30 17:32:03', null),
+('bike-discount.de', 'https://www.bike-discount.de/de/shippingcosts', 4.99, 'WEB_SCRAPING', '2026-03-30 17:32:03', 98.99);
